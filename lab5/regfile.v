95d89d14cb2bec91c0a21eac8c66d7860e592b9d
tangnicholas <nick0207022@gmail.com>
2019-10-03 23:53:06 -0700

    Create regfile.v

---
+++ lab5/regfile.v
@@ -0,0 +1,14 @@
+module regfile(data_in,writenum,write,readnum,clk,data_out);
+  input [15:0] data_in;
+  input [2:0] writenum, readnum;
+  input write, clk;
+  output [15:0] data_out;
+
+
+
+// fill out the rest
+
+
+
+
+endmodule
