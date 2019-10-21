79669b9907e2b24d57800b65558c6622e18dbedc
veronicabcd <49163521+veronicabcd@users.noreply.github.com>
2019-10-20 18:16:15 -0700

    Add files via upload

--- lab6/FSM_chooser.v
+++ lab6/FSM_chooser.v
@@ -1,4 +1,3 @@
-
 `define WAIT 3'b000 
 `define MOVRn 3'b001
 `define MOVRd 3'b010
@@ -16,7 +15,7 @@ module FSM_chooser(clk, s, reset, opcode, op, nsel, w, loada, loadb, loadc, load
   
   output [1:0] nsel, vsel;
   output w, loada, loadb, loadc, loads, asel, bsel, write;
-  reg loada, loadb, loadc, loads, asel, bsel, write, m;
+  reg loada, loadb, loadc, loads, asel, bsel, write, w;
   reg [1:0] nsel, vsel;
   
   reg [4:0] next_state; 
@@ -25,37 +24,37 @@ module FSM_chooser(clk, s, reset, opcode, op, nsel, w, loada, loadb, loadc, load
 
    
   //reg [10:0] chosenOutput = {loada, loadb, loadc, asel, bsel, vsel, write, nsel, m}; //1, 1, 1, 1, 1, 2, 1, 2, m
-  wire mANDs = m & s;
+  wire wANDs = w & s;
   
   muxChooser choose(`MVN, `AND, `CMP, `ADD, `MOVRd, `MOVRn, {opcode, op} , next_state);
   
   always @(posedge clk) begin 
     
-    if (~((next_state === `WAIT & mANDs === 0) | reset === 1)) begin
+    if (~((next_state === `WAIT & wANDs === 0) | reset === 1)) begin
       casex ({next_state, step}) 
-        {`MOVRn, 3'bx}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, m, loads, next_state, step} = {12'bx_x_x_x_x_10_1_00_0_x, `WAIT, 3'bxxx};
+        {`MOVRn, 3'bx}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'bx_x_x_x_x_10_1_00_0_x, `WAIT, 3'bxxx};
         
-        {`MOVRd, 3'd0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, m, loads, next_state, step} = {12'bx_1_1_1_x_10_0_10_0_x, `MOVRd, 3'd1};
-        {`MOVRd, 3'd1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, m, loads, next_state, step} = {12'bx_0_0_0_x_00_1_01_x_x, `WAIT, 3'bxxx};
+        {`MOVRd, 3'd0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'bx_1_1_1_x_10_0_10_0_x, `MOVRd, 3'd1};
+        {`MOVRd, 3'd1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'bx_0_0_0_x_00_1_01_x_x, `WAIT, 3'bxxx};
         
-        {`ADD, 3'd0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, m, loads, next_state, step} = {12'b1_x_x_0_x_10_0_00_0_x, `ADD, 3'd1};
-        {`ADD, 3'd1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, m, loads, next_state, step} = {12'b0_1_x_x_0_10_x_10_x_x, `ADD, 3'd2};
-        {`ADD, 3'd2}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, m, loads, next_state, step} = {12'bx_0_1_x_x_10_x_xx_x_x, `ADD, 3'd3};
-        {`ADD, 3'd3}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, m, loads, next_state, step} = {12'bx_x_0_x_x_00_1_01_x_x, `WAIT, 3'bxxx};
+        {`ADD, 3'd0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b1_x_x_0_x_10_0_00_0_x, `ADD, 3'd1};
+        {`ADD, 3'd1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b0_1_x_x_0_10_x_10_x_x, `ADD, 3'd2};
+        {`ADD, 3'd2}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'bx_0_1_x_x_10_x_xx_x_x, `ADD, 3'd3};
+        {`ADD, 3'd3}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'bx_x_0_x_x_00_1_01_x_x, `WAIT, 3'bxxx};
         
-        {`CMP, 3'd0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, m, loads, next_state, step} = {12'b1_x_x_0_x_10_0_00_0_x, `CMP, 3'd1};
-        {`CMP, 3'd1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, m, loads, next_state, step} = {12'b0_1_x_x_0_10_x_10_x_x, `CMP, 3'd2};
-        {`CMP, 3'd2}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, m, loads, next_state, step} = {12'bx_0_1_x_x_xx_x_xx_x_1, `WAIT, 3'bxxx};
+        {`CMP, 3'd0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b1_x_x_0_x_10_0_00_0_x, `CMP, 3'd1};
+        {`CMP, 3'd1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b0_1_x_x_0_10_x_10_x_x, `CMP, 3'd2};
+        {`CMP, 3'd2}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'bx_0_1_x_x_xx_x_xx_x_1, `WAIT, 3'bxxx};
         
-        {`AND, 3'd0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, m, loads, next_state, step} = {12'b1_x_x_0_x_10_0_00_0_x, `AND, 3'd1};
-        {`AND, 3'd1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, m, loads, next_state, step} = {12'b0_1_x_x_0_10_x_10_x_x, `AND, 3'd2};
-        {`AND, 3'd2}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, m, loads, next_state, step} = {12'bx_0_1_x_x_10_x_xx_x_x, `AND, 3'd3};
-        {`AND, 3'd3}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, m, loads, next_state, step} = {12'bx_x_0_x_x_00_1_01_x_x, `WAIT, 3'bxxx};
+        {`AND, 3'd0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b1_x_x_0_x_10_0_00_0_x, `AND, 3'd1};
+        {`AND, 3'd1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b0_1_x_x_0_10_x_10_x_x, `AND, 3'd2};
+        {`AND, 3'd2}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'bx_0_1_x_x_10_x_xx_x_x, `AND, 3'd3};
+        {`AND, 3'd3}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'bx_x_0_x_x_00_1_01_x_x, `WAIT, 3'bxxx};
         
-        {`MVN, 3'd0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, m, loads, next_state, step} = {12'bx_1_1_1_x_10_0_10_0_x, `MVN, 3'd1};
-        {`MVN, 3'd1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, m, loads, next_state, step} = {12'bx_0_0_0_x_00_1_01_x_x, `WAIT, 3'bxxx};
+        {`MVN, 3'd0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'bx_1_1_1_x_10_0_10_0_x, `MVN, 3'd1};
+        {`MVN, 3'd1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'bx_0_0_0_x_00_1_01_x_x, `WAIT, 3'bxxx};
           
-        default:{loada, loadb, loadc, asel, bsel, vsel, write, nsel, m, loads, next_state, step} = {18'bx};
+        default:{loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {18'bx};
       endcase 
     end
     else begin
@@ -69,8 +68,8 @@ module FSM_chooser(clk, s, reset, opcode, op, nsel, w, loada, loadb, loadc, load
     	nsel  = 0;
       next_state = `WAIT;
       step = 0;
-      m = 1;
       loads = 0;
+      w = 1;
     end
   end           
 endmodule 
