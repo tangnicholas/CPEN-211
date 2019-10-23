9b8bd7714efaef8e4bc2280d62ee28195a64c30d
tangnicholas <nick0207022@gmail.com>
2019-10-23 09:25:40 -0700

    IT WORKS

--- lab6/FSM_chooser.v
+++ lab6/FSM_chooser.v
@@ -1,4 +1,5 @@
-//define the states for finite state  machine
+
+//define the states for finite state machine
 `define WAIT 3'b000 
 `define MOVRn 3'b001
 `define MOVRd 3'b010
@@ -20,81 +21,94 @@ module FSM_chooser(clk, s, reset, opcode, op, nsel, w, loada, loadb, loadc, load
   reg loada, loadb, loadc, loads, asel, bsel, write, w;
   reg [1:0] nsel, vsel; 
   reg [2:0] step;
-  reg [2:0] next_state ;
+  reg [2:0] next_state;
+
+  reg custom; //custom=1 identifies the end of an instruction
 
   wire [2:0] chosenOne; //chosen state from multiplexer
-  wire wANDs = w & s; //used to determine if we are in WAIT state, need to leave WAIT state, or need to go to WAIT
+  wire [2:0] chosenOne_reset;
+  wire wANDs = w & s; //used to determine if we are in WAIT
+
+  assign resetOrcustom = reset | custom;
+ 
+  MuxChooser choose(`MVN, `AND, `CMP, `ADD, `MOVRd, `MOVRn, {opcode, op}, chosenOne); //chooses the next state
+  assign chosenOne_reset = resetOrcustom? `WAIT: chosenOne;
+  //vDFFE #(3) vSelFF(clk, 1'b1, resetOrcustom ? `WAIT : chosenOne, chosenOne_reset); //determines if the next state is WAIT 
+
   
-  //Multiplexer chooses a state, depending on the {opcode,op} input
-  MuxChooser choose(`MVN, `AND, `CMP, `ADD, `MOVRd, `MOVRn, {opcode, op}, chosenOne);
 
-  //reg the chosen state
-  assign chosenOne = next_state;
+  always @(posedge clk) begin
+    next_state = chosenOne_reset;
+    
+    casex ({next_state, step, wANDs}) 
+      {`MOVRn, 3'bx, 1'b1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, custom, step} = {12'b0_0_0_0_0_10_1_00_0_0, 1'b1, 3'd0};
+       
+      {`MOVRd, 3'd0, 1'b1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, custom, step} = {12'b0_1_1_1_0_10_0_10_0_0, 1'b0, 3'd1};
+      {`MOVRd, 3'd1, 1'b0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, custom, step} = {12'b0_0_0_0_0_00_1_01_0_0, 1'b1, 3'd0};
+        
+      {`ADD, 3'd0, 1'b1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, custom, step} = {12'b1_0_0_0_0_10_0_00_0_0, 1'b0, 3'd1};
+      {`ADD, 3'd1, 1'b0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, custom, step} = {12'b0_1_0_0_0_10_0_10_0_0, 1'b0, 3'd2};
+      {`ADD, 3'd2, 1'b0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, custom, step} = {12'b0_0_1_0_0_10_0_10_0_0, 1'b0, 3'd3};
+      {`ADD, 3'd3, 1'b0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, custom, step} = {12'b0_0_0_0_0_00_1_01_0_0, 1'b1, 3'd0};
+        
+      {`CMP, 3'd0, 1'b1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, custom, step} = {12'b1_0_0_0_0_10_0_00_0_0, 1'b0, 3'd1};
+      {`CMP, 3'd1, 1'b0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, custom, step} = {12'b0_1_0_0_0_10_0_10_0_0, 1'b0, 3'd2};
+      {`CMP, 3'd2, 1'b0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, custom, step} = {12'b0_0_1_0_0_10_0_10_0_1, 1'b1, 3'd0};
+        
+      {`AND, 3'd0, 1'b1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, custom, step} = {12'b1_0_0_0_0_10_0_00_0_0, 1'b0, 3'd1};
+      {`AND, 3'd1, 1'b0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, custom, step} = {12'b0_1_0_0_0_10_0_10_0_0, 1'b0, 3'd2};
+      {`AND, 3'd2, 1'b0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, custom, step} = {12'b0_0_1_0_0_10_0_10_0_0, 1'b0, 3'd3};
+      {`AND, 3'd3, 1'b0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, custom, step} = {12'b0_0_0_0_0_00_1_01_0_0, 1'b1, 3'd0};
+        
+      {`MVN, 3'd0, 1'b1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, custom, step} = {12'b0_1_1_1_0_10_0_10_0_0, 1'b0, 3'd1};
+      {`MVN, 3'd1, 1'b0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, custom, step} = {12'b0_0_0_0_0_00_1_01_0_0, 1'b1, 3'd0};
 
-  always @(posedge clk) begin 
-    //on the rising edge of the clock, if we are in state WAIT, w&s != 0 or it is NOT reseting, we go to the next state
-    if (next_state !== `WAIT | wANDs !== 0 | reset === 0) begin
-      casex ({next_state, step}) //evaluates the cases based on the current state and state step
-        {`MOVRn, 3'bx}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b0_0_0_0_0_10_1_00_0_0, `WAIT, 3'd0};
-        
-        {`MOVRd, 3'd0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b0_1_1_1_0_10_0_10_0_0, `MOVRd, 3'd1};
-        {`MOVRd, 3'd1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b0_0_0_0_0_00_1_01_0_0, `WAIT, 3'd0};
-        
-        {`ADD, 3'd0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b1_0_0_0_0_10_0_00_0_0, `ADD, 3'd1};
-        {`ADD, 3'd1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b0_1_0_0_0_10_0_10_0_0, `ADD, 3'd2};
-        {`ADD, 3'd2}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b0_0_1_0_0_10_0_10_0_0, `ADD, 3'd3};
-        {`ADD, 3'd3}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b0_0_0_0_0_00_1_01_0_0, `WAIT, 3'd0};
-        
-        {`CMP, 3'd0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b1_0_0_0_0_10_0_00_0_0, `CMP, 3'd1};
-        {`CMP, 3'd1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b0_1_0_0_0_10_0_10_0_0, `CMP, 3'd2};
-        {`CMP, 3'd2}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b0_0_1_0_0_10_0_10_0_1, `WAIT, 3'd0};
-        
-        {`AND, 3'd0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b1_0_0_0_0_10_0_00_0_0, `AND, 3'd1};
-        {`AND, 3'd1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b0_1_0_0_0_10_0_10_0_0, `AND, 3'd2};
-        {`AND, 3'd2}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b0_0_1_0_0_10_0_10_0_0, `AND, 3'd3};
-        {`AND, 3'd3}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b0_0_0_0_0_00_1_01_0_0, `WAIT, 3'd0};
-        
-        {`MVN, 3'd0}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b0_1_1_1_0_10_0_10_0_0, `MVN, 3'd1};
-        {`MVN, 3'd1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {12'b0_0_0_0_0_00_1_01_0_0, `WAIT, 3'd0};
+      {`WAIT, 3'bxxx, 1'b1}: {loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, custom, step} = {12'b0_0_0_0_0_00_0_00_1_0, 1'b0, 3'd0};
           
-        default:{loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, next_state, step} = {18'bx};
-      endcase 
-    end
-    //else case: current state is WAIT	
-    else begin
-      loada = 1'b0;
-      loadb = 1'b0;
-      loadc = 1'b0;
-      asel = 1'b0;
-      bsel = 1'b0;
-      vsel = 2'b10;
-      write = 1'b0;
-      nsel  = 2'b00;
-      step = 3'd0;
-      loads = 1'b0;
-      w = 1;
-    end
+      default:{loada, loadb, loadc, asel, bsel, vsel, write, nsel, w, loads, custom, step} = {12'b0_0_0_0_0_00_0_00_1_0, 1'b0, 3'd0};
+    endcase 
+
+
+
   end           
 endmodule 
 
-//multiplexer to choose state: chosenOne
+
 module MuxChooser(a5, a4, a3, a2, a1, a0, muxC_in, muxC_out) ;
   parameter k = 3;
   parameter m = 5;
-  input [k-1:0] a5, a4, a3, a2, a1, a0;  // inputs states to pick from
+  input [k-1:0] a5, a4, a3, a2, a1, a0;  // inputs
   input  [m-1:0] muxC_in;          
   output [k-1:0] muxC_out;
   reg [k-1:0] muxC_out;
-    
+
   always @(*) begin
-    case(muxC_in) 
+    case(muxC_in)
       5'b110_10: muxC_out = a0;
       5'b110_00: muxC_out = a1;
       5'b101_00: muxC_out = a2;
       5'b101_01: muxC_out = a3;
       5'b101_10: muxC_out = a4;
       5'b101_11: muxC_out = a5;
-      default: muxC_out = {k{1'bx}} ;
+      default: muxC_out = {k{1'bx}};
     endcase
   end 
-endmodule 
+endmodule
+/*
+//load enabled register 
+module vDFFE(clk, en, din, dout);
+  parameter n = 16;
+  input clk, en;
+  input [n-1:0] din;
+  output [n-1:0] dout;
+  reg [n-1:0] dout;
+  wire [n-1:0] next_out;
+
+  assign next_out = en ? din : dout;
+
+  always @(posedge clk) begin
+    dout = next_out;
+  end
+
+endmodule
+*/
