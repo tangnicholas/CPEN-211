f60d4ddb4c30afb43535902f97bfb76cd9e36108
tangnicholas <nick0207022@gmail.com>
2019-10-23 22:11:25 -0700

    CHANGED AT 9:50

--- lab6/cpu_tb.v
+++ lab6/cpu_tb.v
@@ -35,124 +35,111 @@ module cpu_tb ();
     reset = 0; #10;
     load = 1; in = 16'b1101000000000111; #10; s = 1; load = 0; #10; s=0; #10;
     $display("%b and it should be 7", R0);
-    if (R0 !== 16'b0000000000000111)
-        err = 1'b1;
+    if(R0 !== 16'd7)
+    	err = 1; 
     //MOV2 (move 2 into R1)
     reset = 0; load = 1; in = 16'b1101000100000010; #10; s = 1; load = 0; #10; s=0; #10;
     $display("%b and it should be 2", R1);
-    if (R1 !== 16'b0000000000000010)
-        err = 1'b1;
-      //ADD 
+ 	if(R1 !== 16'd2)
+    	err = 1;
+   //ADD (14+2 = 16)
     reset = 0; load = 1; in = 16'b10100001010_01000; #10; s = 1; load = 0; #10; s=0; #10; //stores in r2
     $display("w is %b, we expect 0 becuase we are in the middle of adding.", w);
     #60;
     //check
-    $display("out is: %b, we expect 0000000000010000", out); 
-
-    //TEST 2 (7 & 2 = 2)
-    reset = 1; #10;
-    //MOVRd 1  (move 7<<1 = 14 into R0)
-    reset = 0; #10;
-    load = 1; in = 16'b110_00_000_000_01_000; #10; s = 1; load = 0; #10; s=0; #20;
-    $display("R0 should now be 14, %b", R0);
-    if (R0 !== 16'b0000000000001110) //R0 should be 14
-        err = 1'b1;
-    //AND1 = (14>>1 & 2, R0=14 R1 = 2)
-    $display("R1 should now be 2, %b", R1); #10;
-    reset = 0; load = 1; in = 16'b101_10_001_001_10_000; #10; s=1; load = 0; #10; s = 0; #20;
-    if (R1 !== 16'b0000000000000010) //out should be 2 given that shifted again (MSB==0), so one was 7 and other was 2. Stores in r1
-        err = 1'b1;
-
-
-    //TEST 3 (14-2 = STATUS)
-    s=0; #10; 
-    //MOV3 (move 2 into R2)
-    reset = 0; load = 1; in = 16'b110_10_010_00000010; #10; s = 1; load = 0; #10; s=0; #20;
-    $display("R2 should now be 2, %b", R2);
-    if (R2 !== 16'b0000000000000010)
-        err = 1'b1;
-    //CMP1 (14-2 = 12(not stored) --> Z = 0, V = 0, w = 0)
-    reset = 0; load = 1; in = 16'b101_01_000_000_00_010; #10; s=1; load = 0; #10; s = 0; #30;
-    $display("out = %b, should be 12", out);
-    $display("Z V w should be 0, 0, 1; %b %b %b", Z, V, w);
-    if (Z !== 1'b0 | V !== 1'b0 | w !== 1)
-        err = 1'b1;
-
-  /*
-    // TEST 4 (14<<1 + 2 = 30) (DOESNT WORK)
-     s = 0;  #10;
-    // MOVRd2 (14<<1 = 28 then store 28 in R0)
-    reset = 0;
-    load = 1; in = 16'b110_00_000_000_01_000; #10; s = 1; load = 0; #10; s=0; #40;
-    $display("R0 should now be 28, %b", R0);
-    if (R0 !== 16'b0000000000011100)
-        err = 1'b1;
-    // ADD2 (28 R0 + 2 R2 = 30)
-      reset = 0; load = 1; in = 16'b101_00_010_010_01_000; #10; s = 1; load = 0; #10; s=0; #60;
-      $display("out is: %b, we expect 0000000000011110", out); 
-       if (out !== 16'b0000000000011110)
-        err = 1'b1;
-*/
-    //TEST 5 (28 & 2 = 0)
-    s=0;  #20;
-    //AND2 (14 R0 --> 28 & 2 R2)
-    reset = 0; load = 1; in = 16'b101_10_001_011_01_000; #10; s = 1; load = 0; #10; s=0; #40;
-    $display("R3 should now be 0s, %b", R3);
-    if (R3 !== 16'b0000000000000000)
-        err = 1'b1;
-
-    //TEST 6  (~14 R0)
-    s=0; #20;
-     //MVN1
-    reset = 0; load = 1; in = 16'b101_11_000_000_00_000; #10; s = 1; load = 0; #10; s=0; #30;
-    $display("R0 should now be ~14, %b", R0);
-    if (R0 !== 16'b1111111111110001)
-        err = 1'b1;
-
- /*
-    //TEST 7  (~2 R2 --> R4) (DOESNT WORK)
-    s=0; reset = 1; #20;
-      //MVN2
-    reset = 0; load = 1; in = 16'b101_11_000_101_00_001; #10; s = 1; load = 0; #10; s=0; #30;
-    $display("R4 should now be ~2, %b", R5);
-    if (R5 !== 16'b1111111111111101)
-      err = 1'b1;
-*/
-
-    //TEST 8 ~(2>>1) 
-    // MVN3
-
-    //TEST 9 (255 + 1=256)
+    $display("out is: %b, we expect 0000000000010000", out);
+    if(R2 !== 16'd16)
+    	err = 1;
+    //AND 
     s=0; #10;
-    //MOV (255 into R2)
-    reset = 0; load = 1; in = 16'b110_10_010_11111110; #10; s = 1; load = 0; #10; s=0; #20;
-    $display("%b and it should be 255", R2);
-    if (R2 !== 16'b0000000011111110)
+    load=1; in = 16'b101_10_000_011_00_001; #10; s=1; load=0; #10; s=0; #40;
+    $display("R3 is %b, should be 2", R3);
+    if(R3 !== 16'd2)
+    	err = 1;
+    //CMP (7-2 =5)
+    s=0; #10;
+    load=1; in = 16'b101_01_000_000_00_001; #10; s=1; load=0; #10; s=0; #40;
+    $display("out = %b, should be 5", out);
+    $display("Z V N w should be 0, 0, 0, 1; %b %b %b %b", Z, V, N, w);
+    if (Z !== 1'b0 | V !== 1'b0 | w !== 1'b1 | N !== 1'b0)
         err = 1'b1;
-/*
-    //MOVrd (2>>1 = 1 into R7)
-    reset = 0; load = 1; in = 16'b110_00_000_111_11_001; #10; s = 1; load = 0; #10; s=0; #20;
-    $display("%b and it should be 1", R7);
-    if (R7 !== 16'b0000000000000001)
-      err = 1'b1;
-    //ADD3 (255 R2 + 1 R3  ---> R1)
-     reset = 0; load = 1; in = 16'b101_10_001_010_01_000; #10; s = 1; load = 0; #10; s=0; #20;
-    if (out !== 16'b0000000100000000)
-        err = 1'b1;
-
-    // TEST 10 (-10 - 255 = STATUS/OVERFLOW)
-      //MOV (-10)
-      reset = 0; load = 1; in = 16'b110_10___; #10; s = 1; load = 0; #10; s=0; #20;
-      $display("%b and it should be -10", R2);
-    if (R2 !== 16'b0000000011111111)
-        err = 1'b1;
-      //CMP
+    //MVN 
+    s=0; #10;
+    load=1; in=16'b101_11_000_100_00_000; #10; s=1; load=0; #10; s=0; #30;
+    $display("R4 is %b, should be ~2 = -3", R4);
+    if(R4 !== 16'b1111111111111101)
+    	err = 1;
     
     
-    // TEST 11(255 - (-10) = STATUS)
-      //CMP
 
-*/
+    //MOV
+    s=0; #10;
+    load = 1; in = 16'b110_10_000_00000011; #10; s = 1; load = 0; #10; s=0; #20;
+    $display("R0 should now be 3, %b", R0);
+    if(R0 !== 16'd3)
+    	err = 1;
+    //ADD
+    s=0; #10;
+    load = 1; in = 16'b101_10_000_010_10_001; #10; s = 1; load = 0; #10; s=0; #40;
+     $display("R2 should now be 1, %b", R2);
+     if(R2 !== 16'd1)
+    	err = 1;
+    //AND
+    s=0; #10;
+    load=1; in = 16'b101_10_000_011_00_001; #10; s=1; load=0; #10; s=0; #60;
+    $display("R3 is %b, should be 2", R3);
+    if(R3 !== 16'd2)
+    	err = 1;
+    //CMP (r0-r1 = 3-2 = 1)
+    s=0; #10;
+    load=1; in = 16'b101_01_010_000_00_000; #10; s=1; load=0; #10; s=0; #40;
+    $display("out = %b, should be -1", out);
+    $display("Z V N w should be 0, 0, 1, 1; %b %b %b %b", Z, V, N, w);
+    if (Z !== 1'b0 | V !== 1'b0 | N !== 1'b1 | w !== 1'b1)
+        err = 1'b1;
+
+    //MVN (R7 = ~2)
+    s=0; #10;
+    load=1; in=16'b101_11_000_111_00_011; #10; s=1; load=0; #10; s=0; #70;
+    $display("R7 is %b, should be ~2", R7);
+    if(R7 !== 16'b1111111111111100)
+    	err = 1;
+
+
+
+    //MOV (r5 = 127)
+    s=0; #10;
+    load=1; in=16'b110_10_101_01111111; #10; s=1; load=0; #10; s=0; #20;
+    $display("R5 is %b, should be 127", R5);
+    if(R5 !== 16'd127)
+    	err = 1;
+    // ADD
+    s=0; #10;
+    load=1; in=16'b101_00_101_110_00_100; #10; s=1; load=0; #10; s=0; #50;
+    $display("R6 is %b, should be 127-3=124", R6);
+    if(R6 !== 16'd124)
+    	err = 1;
+    // CMP
+    s=0; #10;
+    load=1; in=16'b101_01_010_000_00_010; #10; s=1; load=0; #10; s=0; #30;
+    $display("out is %b, should be 1-(1) = 0", out);
+    $display("Z V N w should be 1, 0, 0, 1; %b %b %b %b", Z, V, N, w);
+    if (Z !== 1'b1 | V !== 1'b0 | N !== 0 | w !== 1 )
+        err = 1'b1;
+    //AND
+    s=0; #10;
+    load=1; in=16'b101_10_100_101_00_101; #10; s=1; load=0; #10; s=0; #50;
+    $display("R5 is %b, should be 1111101", R5);
+    if(R5 !== 16'b0000000001111101)
+    	err = 1;
+    //MVN (~127 or -128)
+    s=0; #10;
+    load=1; in=16'b101_11_000_111_10_101; #10; s=1; load=0; #10; s=0; #50;
+    $display("R7 is %b, should be ~127", R7);
+    if(R7 !== 16'b1111111111000000)
+    	err = 1;
+
+
     if (err === 1'b1) begin
       $display("GO BACK AND CHECK WORK.");
     end
