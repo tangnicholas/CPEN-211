471db272d063a5141042b9bf4b94a74c12bc58bb
tangnicholas <nick0207022@gmail.com>
2019-10-27 17:12:45 -0700

    Update lab7_top.v

--- lab7/lab7_top.v
+++ lab7/lab7_top.v
@@ -1,19 +1,5 @@
-`define NUM0 7'b1000000
-`define NUM1 7'b1001111
-`define NUM2 7'b0100100
-`define NUM3 7'b0110000
-`define NUM4 7'b0011001
-`define NUM5 7'b0010010
-`define NUM6 7'b0000010
-`define NUM7 7'b1111000
-`define NUM8 7'b0000000
-`define NUM9 7'b0010000
-`define NUMA 7'b0001000
-`define NUMB 7'b0000011
-`define NUMC 7'b1000110
-`define NUMD 7'b0100001
-`define NUME 7'b0000110
-`define NUMF 7'b0001110
+`define MREAD 2'bxx
+`define MWRITE 2'bxx
 
 module lab7_top(KEY,SW,LEDR,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5);
   input [3:0] KEY;
@@ -21,60 +7,37 @@ module lab7_top(KEY,SW,LEDR,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5);
   output [9:0] LEDR;
   output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
   
+  //Instantiation wire declarations
+  wire [15:0] dout;
+  wire [15:0] write_data;
+  wire [1:0] mem_cmd;
+ 
+  //Tri state declaration and write AND gate
+  wire enable;
+  wire msel;
+  wire write;
+  assign msel = (1'b0 === mem_addr[8:8]);
+  assign enable = (`MREAD === mem_cmd) & msel;
+  assign write =  msel & (`MWRITE === mem_cmd)
+  
+  //Tri-state driver ()
+	assign read_data = enable? dout : 16'bz;
+  
  //instantiating CPU & Read-Write Memory
-  cpu cpu7(.clk(si),
-           .reset(simreset),
-           .s(sims),
-           .load(simload),
-           .in(simin),
-           .out(simout),
-           .N(simN),
-           .V(simV),
-           .Z(simZ),
-           .w(simw));
+  cpu cpu7(.clk(clk),
+           .reset(reset),
+           .s(s),
+           .load(load),
+           .in(in),
+           .out(write_data),
+           .N(N),
+           .V(V),
+           .Z(Z),
+           .w(w)
+           .mem_addr(mem_addr)
+           .mem_cmd(mem_cmd)
+           .read_data(read_data) );
   
-  RAM MEM(.clk(clk),.read_address(read_address),.write_address(write_address),.write(write),.din(din),.dout(dout));
+  RAM MEM(.clk(clk),.read_address(mem_addr[7:0]),.write_address(mem_addr[7:0]),.write(write),.din(write_data),.dout(dout));  
   
-  // fill in sseg to display 4-bits in hexidecimal 0,1,2...9,A,B,C,D,E,F
-  sseg H0(out[3:0], HEX0);
-  sseg H1(out[7:4], HEX1);
-  sseg H2(out[11:8], HEX2);
-  sseg H3(out[15:12], HEX3);
-  assign HEX4 = 7'b1111111;
-  assign {HEX5[2:1], HEX5[5:4]} = 4'b1111; // disabled
-  assign LEDR[8] = 1'b0;
-  
-endmodule
-
-//Tri-state driver ()
-assign output_x = enable? input_x : 'bz;
-
-  
-module sseg(in,segs);
-  input [3:0] in;
-  output [6:0] segs;
-  reg [6:0] segs;
-  
-  // change display to 7-segment display based on input number (binary --> hex)
-  always @*
-    case(in)
-    4'b0000: segs = `NUM0;
-    4'b0001: segs = `NUM1;
-    4'b0010: segs = `NUM2;
-    4'b0011: segs = `NUM3;
-    4'b0100: segs = `NUM4;
-    4'b0101: segs = `NUM5;
-    4'b0110: segs = `NUM6;
-    4'b0111: segs = `NUM7;
-    4'b1000: segs = `NUM8;
-    4'b1001: segs = `NUM9;
-    4'd10: segs = `NUMA;
-    4'd11: segs = `NUMB;
-    4'd12: segs = `NUMC;
-    4'd13: segs = `NUMD;
-    4'd14: segs = `NUME;
-    4'd15: segs = `NUMF;
-    default: segs = {4{1'bx}};
-  endcase
-
 endmodule
