d58965ed086d790f0e9762302a5f4e7fb6c89835
veronicabcd <49163521+veronicabcd@users.noreply.github.com>
2019-11-12 12:11:58 -0800

    Update binary_search.s

--- lab9/binary_search.s
+++ lab9/binary_search.s
@@ -1,11 +1,6 @@
 .globl binary_search
 binary_search:
-	SUB sp, sp, #36 // adjust stack to make room for 9 items
-	STR r12, [sp,#32] // save register r12 for use afterwards
-	STR r11, [sp,#28] // save register r11 for use afterwards
-	STR r10, [sp,#24] // save register r10 for use afterwards
-	STR r9, [sp,#20] // save register r9 for use afterwards
-	STR r7, [sp,#16] // save register r7 for use afterwards
+	SUB sp, sp, #16 // adjust stack to make room for 4 items
 
 	STR r6, [sp,#12] // save register r6 for use KeyIndex
 	STR r5, [sp,#8] // save register r5 for use middleValuez
@@ -23,11 +18,11 @@ binary_search:
 	
 	LDR r5, [r0, r4, LSL #2] //store middleValue to r5
 	CMP r5, r1 //compare middleValue and key
-	BEQ returnsearch  //if middleVlue = key 
 	BGT lowersearch //if middle > key go to uppersearch
 	BLT uppersearch  //if middle < key go to lowersearch 
 	
-	STR r8, [sp,#0]
+    //if middleVlue = key 
+    MOV r6, r4  //KeyIndex = MiddleIndex
 	SUB r8, r8, r8, LSL #1 //numcalls = -numCalls
 	STR r8, [r0, r5, LSL #2] //numbers[middleIndex] = -NumCalls
 	
@@ -46,9 +41,7 @@ gototheend: //returns -1
 	MOV r0, #-1
 	MOV PC, LR
 
-returnsearch:
-	MOV r6, r4	//KeyIndex = MiddleIndex
-						
+				
 
 
 
