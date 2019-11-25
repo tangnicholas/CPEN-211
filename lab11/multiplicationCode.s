2d7692c715cee06d81a28dd90df3bc97ff9b2b68
veronicabcd <49163521+veronicabcd@users.noreply.github.com>
2019-11-25 00:57:53 -0800

    Update multiplicationCode.s

--- lab11/multiplicationCode.s
+++ lab11/multiplicationCode.s
@@ -1,5 +1,4 @@
 //lab 11 part 2
- 
 sums: .double 0.0
 matrix_a: 
 					.double 1.1
@@ -8,20 +7,21 @@ matrix_a:
           .double 2.2
 
 matrix_b: 
-					.double 1
-					.double 2
-          .double 3
-          .double 4
+					.double 1.0
+					.double 2.0
+          .double 3.0
+          .double 4.0
 
-matrix_c:
+matrix_c: .double 0.0
 
+.global _start
+_start: 
 
-
-//matrix multiplication code GOES INSIDE STEP6
-MOV R0, #2 //N
-LDR R1, =matrix_a
-LDR R2, =matrix_b
-LDR R3, =matrix_c 
+MOV R0, #2 // load N
+LDR R1, =matrix_a //loads base address of matrix A
+LDR R2, =matrix_b //loads base address of matrix B
+LDR R3, =matrix_c //loads base address of matrix C
+MOV R6, #0 // sum = 0.0;
 
 MOV R4, #0 // i = 0   OUTER LOOP
 outerLoop: 
@@ -42,18 +42,18 @@ outerLoop:
       MUL R10, R8, R0  //  k*N
       ADD R10, R5, R10, LSL#3 // k*size(row) + j  -->   compute base address --> LSL#3  gives address of B[k][j] 
       ADD R7, R2, R10 // sum = matrix_b + address
-    	.word 0XED97AB00//FLDD R11, [ R7#0]
+    	.word 0XED97AB00 //FLDD R11, [ R7#0]
 
       //sum = sum + A[i][k] * B[k][j];
       LDR R7, =sums
-    	.word 0XED876B00 //FSTD R6, [ R7#0]  store sums into R6
+    	.word 0xDD976B00 //LDR r6, [r7, #0]  
     	.word 0x1BC52F613 //FMULD R7 R9, R11    A[i][k] * B[k][j] in r7
     	.word 0xDE366B07//FADDD R6, R6, R7    R7 = sum + A[i][k] * B[k][j]
       LDR R7, =sums // update sums
     	.word 0XED876B00 //FSTD R6, [ R7#0]  store sums into R6
 
-      ADD R11, R11, #1 //k++
-      CMP R11, R0  // check if k<N to loop again
+      ADD R8, R8, #1 //k++
+      CMP R8, R0  // check if k<N to loop again
       BLT kLoop
 	
 	//C[i][j] = sum;
@@ -64,14 +64,15 @@ outerLoop:
   	ADD R7, R3, R10 // sum = matrix_c + address
 		.word 0XED876B00 //FSTD R6, [ R7#0]  store sums into R6
 
-		ADD R5, R5, #1 //j++
+	ADD R5, R5, #1 //j++
     CMP R5, R0  // check if j<N to loop again
     BLT jLoop
 
 ADD R4, R4, #1 //i++
 CMP R4, R0  // check if i<N to loop again
 BLT outerLoop	
-		
+
+end: B end		
 
 	
 
@@ -83,4 +84,4 @@ BLT outerLoop
 
 
 
-
+ 
